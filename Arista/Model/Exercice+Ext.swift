c9e6f370c25afe938a380bdab4f9e6c3dacdb319
Julien Cotte <Julien.Cotte-CIC@ibm.com>
2025-08-01 09:31:20 +0200

    update login to firstname and lastname in User, add extension to convert String and Int into enul

---
+++ Arista/Model/Exercice+Ext.swift
@@ -0,0 +1,23 @@
+//
+//  Exercice+Ext.swift
+//  Arista
+//
+//  Created by Julien Cotte on 01/08/2025.
+//
+
+import Foundation
+
+extension Exercice {
+
+    /// Used to convert the String from coreData into ExerciceType enum
+    var typeEnum: ExerciceType {
+        get { ExerciceType(rawValue: type ?? "") ?? .other}
+        set { type = newValue.rawValue}
+    }
+    
+    /// Used to convert the Int64 from coreData into Intensity enum
+    var intensityEnum: Intensity {
+        get { Intensity(rawValue: Int(intensity)) ?? .zero}
+        set { intensity = Int64(newValue.rawValue)}
+    }
+}
--- Arista/Model/MealType.swift
+++ Arista/Model/MealType.swift
@@ -5,7 +5,7 @@
 //  Created by Julien Cotte on 31/07/2025.
 //
 
-enum MealType: CaseIterable {
+enum MealType: String, CaseIterable {
     case breakfast
     case lunch
     case dinner
